d3f810cfde13240fa4f106e5a7afd473af8eaa8a
doombubbles <obsidiantoolzg@gmail.com>
2022-04-24 16:27:10 -0700

    Move to new repo

---
+++ BetterArmorPiercingMod.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using Assets.Scripts.Models;
+using Assets.Scripts.Models.Towers;
+using Assets.Scripts.Models.Towers.Mods;
+using BetterArmorPiercing;
+using BTD_Mod_Helper;
+using BTD_Mod_Helper.Api.Enums;
+using BTD_Mod_Helper.Api.Helpers;
+using BTD_Mod_Helper.Api.ModOptions;
+using BTD_Mod_Helper.Extensions;
+using Il2CppSystem.Collections.Generic;
+using MelonLoader;
+
+[assembly:
+    MelonInfo(typeof(BetterArmorPiercingMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace BetterArmorPiercing;
+
+public class BetterArmorPiercingMod : BloonsTD6Mod
+{
+    private static readonly ModSettingInt ArmorPiercingDartsCost = new(3000)
+    {
+        displayName = "Armor Piercing Darts Cost",
+        min = 0,
+        icon = VanillaSprites.ArmorPiercingDartsUpgradeIcon
+    };
+
+    private static readonly ModSettingInt HeatTippedDartsBonus = new(1)
+    {
+        displayName = "Heat Tipped Darts Bonus Damage w/ Armor Piercing",
+        min = 0,
+        max = 10,
+        slider = true,
+        icon = VanillaSprites.HeatTippedDartUpgradeIcon
+    };
+
+    public override void OnNewGameModel(GameModel gameModel, List<ModModel> mods)
+    {
+        gameModel.GetUpgrade(UpgradeType.ArmorPiercingDarts).cost =
+            CostHelper.CostForDifficulty(ArmorPiercingDartsCost, mods);
+
+        foreach (var towerModel in gameModel.GetTowersWithBaseId(TowerType.MonkeySub)
+                     .Where(model => model.appliedUpgrades.Contains(UpgradeType.ArmorPiercingDarts)))
+        {
+            var damageModel = towerModel.GetWeapon()!.projectile.GetDamageModel()!;
+            damageModel.immuneBloonProperties = BloonProperties.None;
+            if (towerModel.appliedUpgrades.Contains(UpgradeType.HeatTippedDarts))
+            {
+                damageModel.damage += HeatTippedDartsBonus;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+<a href="https://github.com/doombubbles/better-armor-piercing/releases/latest/download/BetterArmorPiercing.dll"><img align="right" alt="Download" height="75" src="https://github.com/doombubbles/BTD6-Mods/blob/main/download.png?raw=true"></a>
+
+# Better Armor Piercing
+
+## NOTE: This version of the mod requires BTD6 Mod Helper >v3.0 and MelonLoader >v0.6
+
+This mod makes the x/x/4 Armor Piercing Darts upgrade for the Monkey Sub actually live up to its name by allowing the tower to pop Lead Bloons.
+
+Since this change is more about flavor than literal balance, price of the upgrade is configurable.
+
+Also configurable is a setting for improving the x/2/4+ Heat-tipped Darts cross-path, which would otherwise be obsolete with this mod (default damage bonus is +1).
+
+**Changelog**
+
+v1.2.2 Updated for Mod Helper 3.0
+
+v1.2.1 Updated for new Mod Helper
+
+v1.2.0 Updated for Bloons TD Mod Helper Official Release
